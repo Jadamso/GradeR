33ec9e9b13a0b67e9f7def778c5132ea1fae8626
Jadamso <jordan.m.adamson@gmail.com>
2018-03-23 14:02:19 -0700

    new R commit

--- Code/Make.R
+++ Code/Make.R
@@ -25,25 +25,27 @@ source(paste0(pdir,"Code/PackageSetup.R") )
 # Create R Package Contents
 source(paste0(pdir,"Code/CodeSetup.R") )
 
-pack_up(pdir)
+devtools::build(pkg=packg, manual=TRUE)
+
+system( paste0('R CMD Rd2pdf ', packg) )
+#devtools::check_built(manual=TRUE, path=packg)
 
 #-------------------------------------------------------------------
 ##################
-# Install 
+# Upload and Install 
 ##################
+
+MiscUtils::pack_up(pdir)
+## https://www.rstudio.com/wp-content/uploads/2015/03/devtools-cheatsheet.pdf
+
 devtools::install(packg) ## Locally Works
 
 devtools::install_github( paste0("Jadamso/",pack), subdir=pack)
-## Public Package From Github Fails Often
 
 citation(pack)
 
 print("Done")
 
-## source("~/Desktop/Packages/GradeR/Code/GradeR.R")
-
-## R CMD BATCH --no-save Code/Make.R && rm Make.Rout
-
-#mkdir ~/Desktop/Packages/GradeR/Examples
+## R CMD BATCH --no-save Code/Make.R Code/Make.Rout
 #cp ~/Desktop/Teaching/IntroMacro/Current/Records/{ExamMaker.R,Grades.R,StudentAssignment.R} ~/Desktop/Packages/GradeR/Examples
 
